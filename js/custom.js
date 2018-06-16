abeb4ca905eec6500477c8778f590631880305aa
Yujia-Wang <yujia.wang@hotmail.com>
2018-06-16 11:58:56 +0800

    not done

--- js/custom.js
+++ js/custom.js
@@ -17,31 +17,16 @@ $(document).ready(function() {
     });
 
 
-
     var distance1 = $("#product1").offset().top;
-    var distance2 = $("#product2").offset().top;
-    var distance3 = $("#product3").offset().top;
-    var $window = $(window);
 
-    $window.scroll(function() {
-        if ($window.scrollTop() >= distance1) {
-            $("#product1").css("position", "fixed");
-            $("#text1").css("transform", "translate(-120%, 60%)");
-            $("#pic1").css("transform", "translate(100%, 0)");
-            $("#text2").css("transform", "translate(0, 50%)");
-            $("#pic2").css("transform", "translate(0, 0)");
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= (0.5 * distance1)) {
+            $(".dots").fadeIn(200);
+        } else {
+            $(".dots").fadeOut(200);
         }
-
-        // if ($window.scrollTop() >= distance3) {
-        //     $("#product2").css("position", "fixed");
-        //     $("#product3").fadeIn(3000);
-        // }
-        
-
-        // if ($window.scrollTop() >= distance3) {
-        //     $("#product3").fadeIn(3000);
-        // }
     });
+
 
 
 
